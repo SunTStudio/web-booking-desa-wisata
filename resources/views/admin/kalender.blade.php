697e97c9256c6bdd09118f5e5f7f397bd28bd616
SunStudio <mahsunmuh0@gmail.com>
2024-06-11 15:11:14 +0700

    layout responsif

---
+++ resources/views/admin/kalender.blade.php
@@ -0,0 +1,34 @@
+@extends('admin.layout')
+
+@section('title', 'Kalender | Admin')
+
+@section('titleNav', 'Kalender')
+
+@section('menu')
+    <p class="d-flex justify-content-between align-items-center me-3 "><a href="#" class=" text-secondary fw-bold m-1 ms-3"><i class="bi bi-key me-2  ps-1 pe-1 rounded"></i> Dashboard </a></p>
+    <p class="btn btn-secondary text-light d-flex justify-content-between align-items-center me-3 "><a href="#" class="text-light m-1 ms-3 fw-bold"><i class=" me-2 bi bi-person-fill-up  ps-1 pe-1 rounded"></i> Kalender </a></p>
+    <p class="d-flex justify-content-between align-items-center me-3 "><a href="#" class="text-secondary m-1 ms-3 fw-bold"><i class=" me-2 bi bi-key    ps-1 pe-1 rounded"></i> Booking </a> </p>
+@endsection
+
+@section('content')
+    
+@endsection
+
+@section('menuHp')
+                <div class="col text-center rounded-top bg-secondary">
+                    <a href="" class="text-white"><p><i class="fa-regular fa-calendar-days m-0 p-0 pt-2"></i></p>
+                    <p>Calendar</p></a>
+                </div>
+                <div class="col text-center ">
+                    <a href="" class="text-secondary"><p><i class="fa-solid fa-globe m-0 p-0 pt-2"></i></p>
+                    <p>Dashboard</p></a>
+                </div>
+                <div class="col text-center">
+                    <a href="" class="text-secondary"><p><i class="fa-solid fa-house-lock m-0 p-0 pt-2"></i></p>
+                    <p>Booking</p></a>
+                </div>
+@endsection
+
+@section('scripts')
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin/dashboard');
+});
+
+Route::get('/kalender', function () {
+    return view('admin/kalender');
+});
+
+Route::get('/booking', function () {
+    return view('admin/booking');
 });
