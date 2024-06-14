ce167ee3aec164d4aec9a4d2f15ea159dae06bcb
MIqbalNP <miqbalnp@students.amikom.ac.id>
2024-06-14 11:24:53 +0700

    landing-form

---
+++ resources/views/form.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Form Booking</h1>
+        <form action="">
+            <div class="mb-3">
+                <label for="" class="mb-2">Tanggal Visitor</label>
+                <input type="text" placeholder="Masukan tanggal YYYY-MM-DD" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label for="" class="mb-2">Nama PIC</label>
+                <input type="text" placeholder="Masukan Nama" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label for="" class="mb-2">No. Telp PIC</label>
+                <input type="text" placeholder="Masukan No. Telp" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label for="" class="mb-2">Jam Mulai</label>
+                <input type="time" placeholder="Masukan Jam Mulai" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label for="" class="mb-2">Jam Selesai</label>
+                <input type="time" placeholder="Masukan Jam Selesai" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label for="" class="mb-2">Jumlah Visitor</label>
+                <input type="text" placeholder="Masukan Jumlah Visitor" class="form-control">
+            </div>
+            <button type="submit" class="btn btn-primary">Booking Tanggal</button>
+        </form>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
